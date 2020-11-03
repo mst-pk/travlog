cf2ea3c7a0f4ed3e52212ec8931f1bf15d94ba6e
masato.kinoshita <mst.supertramp99@gmail.com>
2020-11-03 04:54:26 +0000

    Fix events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,15 +2,26 @@ class EventsController < ApplicationController
     before_action :require_logged_in
     
     def index
+        @events = current_user.events.order(id: :desc).page(params[:page])
     end
     
     def show
+        @event = current_user.events
     end
     
     def new
+        @event = current_user.events.build
     end
     
     def create
+        @event = current_user.events.build(event_params)
+        if @event.save
+            flash[:success] = "投稿しました。"
+            redirect_to events_url
+        else
+            flash.now[:danger] = "投稿に失敗しました。"
+            render :new
+        end
     end
     
     def edit
@@ -21,4 +32,9 @@ class EventsController < ApplicationController
     
     def destroy
     end
+    
+    private
+        def event_params
+            params.require(:event).permit(:title, :comment, :event_date, :address, :latitude, :longitude)
+        end
 end
