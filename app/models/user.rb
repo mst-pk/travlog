7e081148de1a7016e8cbc15b2c6cb7c8ba4ac2d1
masato.kinoshita <mst.supertramp99@gmail.com>
2020-11-21 10:56:28 +0000

    Fix user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_many :travels, dependent: :destroy
     has_many :relationships, dependent: :destroy
     has_many :followings, through: :relationships, source: :follow
-    has_many :reverse_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+    has_many :reverse_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
     has_many :followers, through: :reverse_relationships, source: :user
     has_many :likes, dependent: :destroy
     has_many :like_events, through: :likes, source: :event
